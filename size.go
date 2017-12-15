d6eab25d7923d9becd97fda3a697feb7972c56bc
Ian Denhardt <ian@zenhack.net>
2017-12-14 21:26:39 -0500

    Split wrappers into size/write
    Will add a `read` soonish.

---
+++ size.go
@@ -0,0 +1,13 @@
+package wayland
+
+func sizeOf_new_id(Object) uint16 { return 4 }
+func sizeOf_int(int32) uint16     { return 4 }
+func sizeOf_uint(uint32) uint16   { return 4 }
+func sizeOf_Fixed(Fixed) uint16   { return 4 }
+func sizeOf_object(Object) uint16 { return 4 }
+func sizeOf_fd(int) uint16        { return 0 }
+
+func sizeOf_string(s string) uint16 {
+	// XXX: we need to make sure this doesn't overflow somehow.
+	return uint16(4 + ceil32(len(s)+1))
+}
