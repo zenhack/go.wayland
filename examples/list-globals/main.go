769f6afbd0b49c9c1815ca474bd33c53cd22fe5d
Ian Denhardt <ian@zenhack.net>
2017-12-23 20:00:04 -0500

    Example: exit.

--- examples/list-globals/main.go
+++ examples/list-globals/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"zenhack.net/go/wayland"
 )
@@ -18,11 +19,17 @@ func main() {
 	display := conn.GetDisplay()
 	display.OnError(func(oid wayland.ObjectId, code uint32, message string) {
 		fmt.Printf("error from server: (%d, %d, %q)\n", oid, code, message)
+		os.Exit(1)
 	})
 	registry, err := display.GetRegistry()
 	chkfatal(err)
 	registry.OnGlobal(func(name uint32, iface string, version uint32) {
 		fmt.Printf("new global: (%d, %s, %d)\n", name, iface, version)
+	})
+	cb, err := display.Sync()
+	chkfatal(err)
+	cb.OnDone(func(uint32) {
+		os.Exit(0)
 	})
 	chkfatal(conn.MainLoop())
 }
